73a401a95dd01a9c4735730524bbfa71362c1b30
mdSoroarJahan <soroarjahan17@gmail.com>
2026-01-03 03:52:18 +0600

    Implement resourceful Category and Post controllers, update migration files to include necessary fields

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'content',
+        'feature_image',
+        'category_id',
+        'user_id'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
