921d83bf5f73614c62fd78e41e99204feccbbc89
mdSoroarJahan <soroarjahan17@gmail.com>
2026-01-03 02:20:29 +0600

    Implement authentication routes and views
    - Updated web.php to include routes for registration, login, and logout.
    - Created register.blade.php for user registration with form validation.
    - Created login.blade.php for user login (currently empty).
    - Added app.blade.php layout with navigation for authenticated and guest users.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('layouts/app');
+})->name('home');
+
+Route::get('/register', [AuthController::class, 'registrationPage'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/login', [AuthController::class, 'loginPage'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
