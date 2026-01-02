627ccca6af295a0e0f83c71924928020300a3bef
mdSoroarJahan <soroarjahan17@gmail.com>
2026-01-02 23:35:56 +0600

    Add authentication and migration setup for categories and posts

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function registrationPage()
+    {
+        return view('auth.register');
+    }
+    public function register(Request $request)
+    {
+        $request->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|confirmed'
+        ]);
+
+        User::create([
+            'name' => $request->request,
+            'email' => $request->email,
+            'password' => $request->password
+        ]);
+
+        Auth::attempt($request->only('email', 'password'));
+        return redirect()->route('dashboard');
+    }
+
+    public function loginPage()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'), $request->remember)) {
+            return back()->with('status', 'Invalid login details');
+        }
+
+        return redirect()->route('dashboard');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('login');
+    }
+}
